b46cf9ec9a7694b7ce66dc878f0886f1ee80b0f5
Vladyslav Domariev <vlad.domariev@gmail.com>
2024-11-17 20:07:08 +0100

    Added check winner logic

--- tutorial-tic-tac-toe/src/component/board/Board.js
+++ tutorial-tic-tac-toe/src/component/board/Board.js
@@ -5,10 +5,17 @@ export function Board() {
 
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const winner = calculateWinner(squares);
+    let gameStatus = "";
+    if (winner) {
+        gameStatus = "Winner: " + winner;
+    } else {
+        gameStatus = "Next player: " + (xIsNext ? "X" : "O");
+    }
 
     function handleClick(indexSqueare) {
         const nextSquares = squares.slice();
-        if (nextSquares[indexSqueare]) {
+        if (nextSquares[indexSqueare] || calculateWinner(squares)) {
             return;
         }
 
@@ -17,8 +24,8 @@ export function Board() {
         setXIsNext(!xIsNext);
     }
 
-    let row = [];
-    let j = 0;
+    // let row = [];
+    // let j = 0;
     // for (let i = 0; i < 3; i++) {
     //     row.push(<Square value={squares[j]} onSquareClick={() => handleClick(0 + j)}></Square>);
     //     j++;
@@ -29,6 +36,8 @@ export function Board() {
         <div className="board-row">{row}</div>
         <div className="board-row">{row}</div>
          */}
+
+        <div className="status">{gameStatus}</div>
         <div className="board-row">
             <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
             <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -47,4 +56,24 @@ export function Board() {
     </>;
 
 
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 }
