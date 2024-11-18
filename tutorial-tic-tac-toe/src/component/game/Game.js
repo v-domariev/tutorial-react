e9ba0a395c96726d359280f7b2e7e3ecdf06b4bc
Vladyslav Domariev <vlad.domariev@gmail.com>
2024-11-18 13:44:05 +0100

    Back to step

--- tutorial-tic-tac-toe/src/component/game/Game.js
+++ tutorial-tic-tac-toe/src/component/game/Game.js
@@ -14,13 +14,41 @@ export function Game() {
         setXIsNext(!xIsNext);
     }
 
+    function jumpTo(nextMove) {
+        if (history.length == 0) {
+            return;
+        }
+        let xIsNextCopy = xIsNext;
+        for (let i = history.length - nextMove; i >= 0; i--) {
+            xIsNextCopy = !xIsNextCopy;
+        }
+        setXIsNext(xIsNextCopy);
+        const historyJumpedTo = history.slice(0, nextMove);
+        setHistory(historyJumpedTo);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+
+    });
+
     return (
         <div className="game">
             <div className="game-board">
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
 
